c63c9aeb81ea16b448fad48380384899ac206492
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-05-31 21:47:25 +0600

    up

--- src/components/NovelCard.jsx
+++ src/components/NovelCard.jsx
@@ -1,6 +1,9 @@
-export default function NovelCard({ title, img }) {
+export default function NovelCard({ title, img, to }) {
   return (
-    <div className="
+    <a
+    href={to}
+    >
+      <div className="
         bg-white
         shadow
         rounded-md
@@ -9,25 +12,25 @@ export default function NovelCard({ title, img }) {
         hover:shadow-lg
         transition
         hover:scale-105
-
+        cursor-pointer
         
 
     ">
-      <img
-        src={img}
-        alt="novel cover"
-        className="
+        <img
+          src={img}
+          alt="novel cover"
+          className="
         rounded-md
         hover:opacity-75
         transition
         hover:scale-105
         "
-        style={{
-          height: "300px",
-          width: "250px",
-        }}
-      />
-      <span className="
+          style={{
+            height: "300px",
+            width: "250px",
+          }}
+        />
+        <span className="
        absolute
        top-0
          right-0
@@ -37,10 +40,10 @@ export default function NovelCard({ title, img }) {
          rounded
 
         ">
-        New
-      </span>
-      <div
-        className="
+          New
+        </span>
+        <div
+          className="
         text-white
         absolute
         bottom-4
@@ -53,22 +56,23 @@ export default function NovelCard({ title, img }) {
        
              
         "
-        style={{
-          fontFamily: "Roboto",
-          background: "rgba(255, 255, 255, 0.25)",
-          borderadius: 16,
-          boxShadow: " 0 4px 30px rgba(0, 0, 0, 0.1)",
-          backdropFilter: "blur(5px)",
-          WebkitBackdropFilter: "blur(5px)",
-          border: "1px solid rgba(255, 255, 255, 0.3)",
-        }}
-      >
-        <p>Status : Completed</p>
-        <p>
-          Chapter : 240 Total Views : 12k
-        </p>
-        <p>{title}</p>
+          style={{
+            fontFamily: "Roboto",
+            background: "rgba(255, 255, 255, 0.25)",
+            borderadius: 16,
+            boxShadow: " 0 4px 30px rgba(0, 0, 0, 0.1)",
+            backdropFilter: "blur(5px)",
+            WebkitBackdropFilter: "blur(5px)",
+            border: "1px solid rgba(255, 255, 255, 0.3)",
+          }}
+        >
+          <p>Status : Completed</p>
+          <p>
+            Chapter : 240 Total Views : 12k
+          </p>
+          <p>{title}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
