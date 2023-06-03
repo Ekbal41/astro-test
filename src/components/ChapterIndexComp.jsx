4549855499370be03ae488d695dec20825bd1993
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-06-04 01:30:40 +0600

    lol

---
+++ src/components/ChapterIndexComp.jsx
@@ -0,0 +1,101 @@
+export default function ChapterIndexComp() {
+  return (
+    <>
+      <div class="mx-20 mt-8 bg-white rounded-t-md shadow-md p-8">
+        <div class="flex justify-between mb-8 ">
+          <select class="bg-sky-100 py-2 px-3 caret-sky-500 shadow-sm rounded-md text-gray-700 capitalize">
+            <option value="100-200">100-200</option>
+            <option value="100-200">100-200</option>
+            <option value="100-200">100-200</option>
+            <option value="100-200">100-200</option>
+            <option value="100-200">100-200</option>
+            <option value="100-200">100-200</option>
+          </select>
+          <input class="bg-sky-100 py-2 px-3 shadow-sm rounded-md text-gray-700 capitalize" placeholder="Search chapter..." />
+        </div>
+        <div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 1 : Epilogue
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 2 : Shaking the Heavens
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 3 : Primodial Essence
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 4 : The Dragon Prince
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 1 : Epilogue
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 2 : Shaking the Heavens
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 3 : Primodial Essence
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 4 : The Dragon Prince
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 1 : Epilogue
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 2 : Shaking the Heavens
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 3 : Primodial Essence
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 4 : The Dragon Prince
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 1 : Epilogue
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 2 : Shaking the Heavens
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 3 : Primodial Essence
+            </div>
+          </div>
+          <div class="border-b-2 cursor-pointer  transition-colors">
+            <div class="bg-gray-100 p-4 text-gray-800 hover:bg-gray-200 hover:underline">
+              Chapter 4 : The Dragon Prince
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
