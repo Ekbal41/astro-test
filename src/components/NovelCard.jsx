8705ae61a0e40f7b4c1cb2389d2654d51f732051
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-06-11 15:18:06 +0600

    45

--- src/components/NovelCard.jsx
+++ src/components/NovelCard.jsx
@@ -11,19 +11,19 @@ export default function NovelCard() {
           <a href="/novel-details" className="">Nine Star Hegemon Body Art</a>
         </div>
         <div className="flex gap-2 text-yellow-500">
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
-          <i class="bi bi-star-fill"></i>
+          <i className="bi bi-star-fill"></i>
+          <i className="bi bi-star-fill"></i>
+          <i className="bi bi-star-fill"></i>
+          <i className="bi bi-star-fill"></i>
+          <i className="bi bi-star-fill"></i>
           (4.5)
         </div>
         <h4 className="capitalize">
-          <i class="bi bi-stickies mr-2"></i>chapter 1124{" "}
+          <i className="bi bi-stickies mr-2"></i>chapter 1124{" "}
         </h4>
 
         <p className="text-gray-400 text-sm">
-          <i class="bi bi-clock text-slate-800 mr-2 text-sm"></i> 2 days ago
+          <i className="bi bi-clock text-slate-800 mr-2 text-sm"></i> 2 days ago
         </p>
       </div>
     </div>
--- src/components/ReaderX.jsx
+++ src/components/ReaderX.jsx
@@ -1,22 +1,74 @@
+import { useState } from "react";
+
 export default function ReaderX() {
+  const [theme, setTheme] = useState("white");
+  const [fontSize, setFontSize] = useState(20);
   return (
     <>
-      <div className="xl:p-8">
+      <div
+        className="xl:p-8 "
+        style={{
+          backgroundColor: theme === "dark" ? "#0e0f0f" : "rgb(243,244,246)",
+        }}
+      >
         <div className="grid grid-cols-12">
-          <div className="col-span-0 xl:col-span-1">
-          </div>
-          <div className="col-span-12 xl:col-span-8 bg-white p-4 mt-12">
-            <div className="xl:mb-20 xl:mt-12 xl:ml-12 mb-8">
-              <a
-                href="/novel"
-                className="text-3xl font-bold text-slate-700 cursor-pointer hover:underline"
+          <div
+            className="col-span-0 md:col-span-12 xl:col-span-1 p-4"
+            style={{
+              color: theme === "dark" ? "#999999" : "#272727",
+            }}
+          >
+            <div className="flex flex-row gap-2 xl:flex-col">
+              <button
+                onClick={() => setTheme("white")}
+                className="bg-white border p-4 h-6 md:h-10 w-10 rounded-full"
               >
-                A Will Eternal
-              </a>
-              <p className="text-slate-400">Chapter 4 : The Dragon Prince</p>
+              </button>
+              <button
+                className=" p-4 h-6 md:h-10 w-10 rounded-full border"
+                onClick={() => setTheme("dark")}
+                style={{
+                  backgroundColor: "#272727",
+                }}
+              >
+              </button>
+              <button className="bg-indigo-200 p-4  h-6 md:h-10 w-10 rounded-full border">
+              </button>
+              <button className="bg-cyan-200 p-4 h-6 md:h-10 w-10 rounded-full border">
+              </button>
+              <button
+                onClick={() => setFontSize(fontSize + 1)}
+                className=" text-2xl md:mt-2  rounded-full flex justify-center item-top font-bold lg:mr-4"
+              >
+                A+
+              </button>
+              <button
+                onClick={() => setFontSize(fontSize - 1)}
+                className="text-2xl md:mt-2 rounded-full flex justify-center item-center font-bold lg:mr-4"
+              >
+                A-
+              </button>
+            </div>
+          </div>
+          <div
+            className="col-span-12 xl:col-span-10  p-4 mb-12 "
+            style={{
+              backgroundColor: theme === "dark" ? "#272727" : "#fff",
+              color: theme === "dark" ? "#999999" : "#272727",
+              fontSize: fontSize,
+            }}
+          >
+            <div className="xl:mb-20 xl:mt-12 xl:ml-12 mb-8">
+              <p className="font-bold">Chapter 4 : The Dragon Prince</p>
             </div>
 
-            <div className="xl:ml-12 xl:mr-8 font-sans text-xl leading-9 text-slate-800">
+            <div
+              className="xl:ml-12 xl:mr-8 font-sans text-xl leading-9 "
+              style={{
+                fontSize: fontSize,
+                lineHeight: "1.8",
+              }}
+            >
               The spectacle of many gods appearing in the Sea Realm last time
               might have occurred during the last ominous battle.<br />
               <br />{" "}
@@ -56,146 +108,12 @@ export default function ReaderX() {
               be willing to choose a successor and try to pass on all your
               knowledge.<br />
               <br />” Ads by Pubfuture A god said, “Since the Primordial Era,
-              the Path of Reincarnation has been severed.<br />
-              <br />{" "}
-              No one in the Age of the Gods dares to say that they can enter it,
-              but now you said you could help us enter it within a thousand
-              years.<br />
-              <br />{" "}
-              Boy, have you learned any news about the Path of Reincarnation?”
-              Han Fei wondered if he should tell them about the Reincarnation
-              Road.<br />
-              <br /> However, this was a serious matter.<br />
-              <br />{" "}
-              If he told them that the Reincarnation Road was with him, it might
-              cause trouble.<br />
-              <br />
-              Han Fei said proudly, “Apart from being Human Emperor, I am also a
-              disciple of the Void Temple.<br />
-              <br />{" "}
-              I don't know if you know of the Void Temple, but as far as the
-              Eternal Race are concerned, they don't dare to mess with our Void
-              Temple.<br />
-              <br /> I can reveal some information to you.<br />
-              <br />
-              The Void Temple has already grasped some secrets about the Path of
-              Reincarnation.<br />
-              <br />{" "}
-              I wonder if this information can make you give up giving them your
-              inheritances?” Buzz ~ Buzz ~ For a moment, the gods' momentum
-              soared to the sky, their divine persona collided with each other,
-              and they seemed excited.<br />
-              <br /> “We know about the Void Temple.<br />
-              <br />” “Han Fei, you've just grasped some secrets about the Path
-              of Reincarnation.<br />
-              <br />
-              Are you sure that you can reopen the Path of Reincarnation in a
-              thousand years?” Han Fei said indifferently, “The Void Temple has
-              never lied.<br />
-              <br />{" "}
-              Now, the ominous is stirring, and some are already
-              returning.<br />
-              <br />{" "}
-              But now, the gods in the Sea Realm and even the Sea of Stars have
-              withered.<br />
-              <br />{" "}
-              Do you think the current Sea Realm can resist the ominous?” “What?
-              The ominous has returned?” Han Fei said, “Before I came to the
-              Graveyard of Gods, I had killed hundreds of thousands of
-              destruction-level ominous creatures.<br />
-              <br />” Ads by Pubfuture “The time won't be enough.<br />
-              <br />” “No, there's still time.<br />
-              <br />{" "}
-              It will take at least tens of thousands of years for the
-              dominator-level ominous creatures to return.<br />
-              <br />{" "}
-              However, if this is the case, when the Path of Reincarnation
-              opens, we can't take a new path.<br />
-              <br />{" "}
-              We have to take our own old paths and return to our original
-              divine position.<br />
-              <br /> That way, there's still time.<br />
-              <br />” “It's all that damn Eternal Race's fault.<br />
-              <br />{" "}
-              If I return to my divine position after reincarnation, I'll
-              definitely fight this race to the death.<br />
-              <br />” The gods discussed animatedly, and the goddess' eyes
-              flickered as if she was thinking about something else.<br />
-              <br />{" "}
-              Han Fei said, “Seniors, apart from the people of the Void Temple,
-              only you know this news.<br />
-              <br />
-              Apart from that, no third party knows.<br />
-              <br />{" "}
-              I'm telling you this because firstly, I want you to be mentally
-              prepared.<br />
-              <br />{" "}
-              Secondly, most of those people just now came from the Central Sea
-              Divine Realm.<br />
-              <br />{" "}
-              These people slaughtered 70 billion people of our human
-              race.<br />
-              <br /> I, Han Fei, am irreconcilable with them.<br />
-              <br />{" "}
-              As the saying goes, one has to be safe inside when fighting
-              outsiders.<br />
-              <br /> Therefore, I must kill these people.<br />
-              <br />
-              Not just these people, I will kill my way into the Central Sea
-              Divine Realm sooner or later.<br />
-              <br />{" "}
-              Therefore, it's not that I don't give you face, but they are not
-              worthy of inheriting your inheritances.<br />
-              <br />” At this moment, killing intent surged from Han Fei.<br />
-              <br /> The gods knew that Han Fei was serious.<br />
-              <br />{" "}
-              A god declared, “This matter will never reach the ears of a third
-              party.<br />
-              <br />{" "}
-              The Graveyard of Gods is not a place anyone can come to.<br />
-              <br />{" "}
-              If the Path of Reincarnation can be reopened in only a thousand
-              years, we won't sleep anymore.<br />
-              <br /> A mere thousand years is nothing to us.<br />
-              <br />” A god echoed, “We are all people waiting for death.<br />
-              <br />{" "}
-              We will definitely not tell others about the Path of
-              Reincarnation.<br />
-              <br />” Han Fei said, “Of course I trust you, seniors.<br />
-              <br /> Otherwise, I wouldn't have told you this.<br />
-              <br />” A god said, “Little friend Han Fei, your human race has
-              lost 70 billion lives and you want to avenge them.<br />
-              <br /> We can understand your feelings.<br />
-              <br />{" "}
-              However, can you be sure that these people are all your enemies?
-              There are always good and evil creatures in this world.<br />
-              <br />
-              Although these people are not as talented as you, they have made
-              it here with luck, strength, and temperament.<br />
-              <br />{" "}
-              Do you want to kill them all?” Han Fei stroked his chin.<br />
-              <br />{" "}
-              “If you insist on choosing your inheritors from these people,
-              please set a test for them.<br />
-              <br />{" "}
-              Anyone who is hostile to our human race, can't take the
-              inheritances! Because sooner or later, I will kill them.<br />
-              <br />{" "}
-              By the way, the people who came in with me grew up with me and
-              became strong with me.<br />
-              <br />{" "}
-              They have cultivated for less than a thousand years but have are
-              become peerless Heavenly Talents.<br />
-              <br />{" "}
-              I believe even if I don't say it, you should be able to feel their
-              talent.<br />
-              <br />”
             </div>
           </div>
-          <div className="col-span-0 xl:col-span-3">
+          <div className="col-span-0 xl:col-span-1">
           </div>
         </div>
-        <div className="flex justify-center gap-4">
+        <div className="flex justify-center gap-4 pb-12">
           <a
             href="/reader"
             className="inline-flex items-center uppercase font-bold bg-green-500 text-white border-0 py-1 px-3 focus:outline-none hover:bg-green-700 rounded text-base mt-4 md:mt-0"
